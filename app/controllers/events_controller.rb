648de05cc578342eee23137bd157849a024120de
Chris Chiu <hungyichiu@gmail.com>
2019-09-11 17:38:29 +0800

    add controller Event with index new show

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,10 @@
+class EventsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # get 'events/index'
+  # get 'events/new'
+  # get 'events/show'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :events
+
+  root 'events#index'
 end
