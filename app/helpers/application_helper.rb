ee33e92f2fb3c72db04371653d497ac7d68879fa
anne <anneju.824@gmail.com>
2019-09-24 15:49:17 +0800

    completed avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  # def avatar_tag(user, class_name: 'avatar', size: '100x100')
-  #   # if user.avatar.attached?
-  #   #   image_tag user.avatar.variant(resize: size ), class: class_name
-  #   # else
-  #   #   image_tag "#{Rails.root}/public/pictures/default_avatar.png", class: class_name
-  #   # end
+  def avatar_tag(user, class_name: 'avatar', size: '100x100')
+    if user.avatar.attached?
+      image_tag user.avatar.variant(resize: size), class: class_name
+    else
+      image_tag "#{Rails.root}/public/pictures/default_avatar.png", class: class_name
+    end
     
-  # end
+  end
 end
