8be8f53e2c408333c8b54bca2555427f9415135a
Chris Chiu <hungyichiu@gmail.com>
2019-09-25 14:26:07 +0800

    設定登入後導回登入前頁面，資料庫找不到資料時導向404頁面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   after_action :store_action
+  
+
+  private
+  def record_not_found
+    render file: "#{Rails.root}/public/404.html", 
+           status: :not_found,
+           layout: false
+  end
   
   def store_action
     return unless request.get? 
