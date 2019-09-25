be5463d33e1cdfa0a42960f5713baf9bb3fb6c5e
anne <anneju.824@gmail.com>
2019-09-25 18:07:01 +0800

    manual fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   has_many :event_logs
   has_many :events, through: :event_logs
   enum gender: { "男": 0, "女": 1 }
