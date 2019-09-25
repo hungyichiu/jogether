30540da3a225b36ed242bb6b10f845f5c2fc10c8
Chris Chiu <hungyichiu@gmail.com>
2019-09-25 09:05:29 +0800

    events 新增 description 欄位

---
+++ db/migrate/20190924232248_add_column_to_event.rb
@@ -0,0 +1,5 @@
+class AddColumnToEvent < ActiveRecord::Migration[5.2]
+  def change
+    add_column :events, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_020937) do
+ActiveRecord::Schema.define(version: 2019_09_24_232248) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_09_24_020937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "location"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
