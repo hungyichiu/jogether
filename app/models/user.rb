c5de24e268972a75125171b8d4141868fc36556d
Chris Chiu <hungyichiu@gmail.com>
2019-09-25 14:45:12 +0800

    fixed Model ORM setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :events
-  enum gender: { "女": 0, "男": 1 }
+  has_many :event_logs
+  has_many :events, through: :event_logs
+  enum gender: { "男": 0, "女": 1 }
   enum user_level: { normal: 0, admin: 1 }
   has_one_attached :avatar
 end
