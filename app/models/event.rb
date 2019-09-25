6daa9d480d26b984dcea2b68488cfc332c2a1138
anne <anneju.824@gmail.com>
2019-09-25 13:47:30 +0800

    photo finish

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,5 @@ class Event < ApplicationRecord
   has_many :users, through: :event_logs
   enum event_type: { "運動": 0, "美食": 1, "藝文": 2, "娛樂": 3, "學習": 4 }
   enum event_status: { before_post: 0, posted: 1, closed: 2, rejected: 3 }
+  has_one_attached :image
 end
