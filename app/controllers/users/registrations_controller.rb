6a0c37c5edf6314642ad50d4afb555fc0ad5269b
Chris Chiu <hungyichiu@gmail.com>
2019-09-26 16:43:25 +0800

    fixed user_info update failed

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -21,6 +21,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # PUT /resource
   # def update
+  #   byebug
   #   super
   # end
 
@@ -47,7 +48,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys:[:name, :age, :gender, :mobile, :ser_level,  :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name, :age, :gender, :mobile, :user_level,  :avatar])
   end
 
   # update without current password
