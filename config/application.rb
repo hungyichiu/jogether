70420e71effa97a4c691c39e565c167fc4858126
anne <anneju.824@gmail.com>
2019-10-03 14:23:32 +0800

    try fix Dotenv

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-Dotenv::Railtie.load
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
 
 module Jogether
   class Application < Rails::Application
