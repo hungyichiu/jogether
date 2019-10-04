67af4f7eeb75094f668c3521e773c7edcb934edc
bunnyy <joewlozg@gmail.com>
2019-10-04 09:52:00 +0800

    updateRegistrationV1

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :disable_nav, only: [:new]
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +25,10 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  private
+
+  def disable_nav
+    @disable_nav = true
+  end
 end
