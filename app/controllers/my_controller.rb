6a07da890781870806e3738d6f4557a272a203d5
anne <anneju.824@gmail.com>
2019-10-07 14:37:48 +0800

    save

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -6,7 +6,7 @@ class MyController < ApplicationController
 
   def like
     @events = current_user.like_events
-    HardWorkJob.set( wait: 20.seconds ).perform_later(current_user.id)
+    HardWorkJob.delay.go(current_user.id)
   end
 
   def events_applied
--- app/jobs/hard_work_job.rb
+++ app/jobs/hard_work_job.rb
@@ -1,6 +1,11 @@
 class HardWorkJob < ApplicationJob
+  queue_as :default
 
   def perform(user_id)
+    user = User.find(user_id)
+  end
+
+  def self.go(user_id)
     user = User.find(user_id)
   end
 end
